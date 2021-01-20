8c5ed5c5acb7a72a46bfe776e8fd505c38750639
Asandra <64925856+Asandra-saratov@users.noreply.github.com>
2021-01-20 20:23:37 +0400

    правки пробелов js

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,15 +1,15 @@
-  var navMain = document.querySelector('.main-nav');
-  var navToggle = document.querySelector('.toggle');
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.toggle');
 
-  navMain.classList.remove('main-nav--nojs');
-  navToggle.classList.remove('toggle--nojs');
+navMain.classList.remove('main-nav--nojs');
+navToggle.classList.remove('toggle--nojs');
 
-  navToggle.addEventListener('click', function () {
-    if (navToggle.classList.contains('toggle--closed')) {
-      navToggle.classList.remove('toggle--closed');
-      navMain.classList.add('main-nav--opened');
-    } else {
-      navToggle.classList.add('toggle--closed');
-      navMain.classList.remove('main-nav--opened');
-    }
-  });
+navToggle.addEventListener('click', function () {
+  if (navToggle.classList.contains('toggle--closed')) {
+    navToggle.classList.remove('toggle--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navToggle.classList.add('toggle--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
